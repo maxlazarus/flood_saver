bc6547c5a03511f5f6e9345d1d75e8a5f3ffa665
maxlazarus <max.prokopenko@gmail.com>
2017-08-20 23:30:04 -0400

    Debugging PLC logic

--- FloodSaver/FloodSaver.ino
+++ FloodSaver/FloodSaver.ino
@@ -9,18 +9,31 @@
 #include <avr/wdt.h>
 #include <avr/delay.h>
 
-const byte rs = 12, en = 11, d4 = 10, d5 = 9, d6 = 8, d7 = 7;
-const byte 
-	button_0_pin = 2, 
-	button_1_pin = 3, 
-	led_red_pin = 4, 
-	led_green_pin = 5, 
-	buzzer_pin = 6,
-	pressure_pin = A0;
-byte button_0, button_1;
-uint16_t pressure_reading_deciP;
-bool button_0_pressed, button_1_pressed;
+const byte
+	// LCD pins
+	rs	= 12, 
+	en	= 11, 
+	d4	= 10, 
+	d5	= 9, 
+	d6	= 8, 
+	d7	= 7;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+const byte 
+	// System IO pins
+	button_0_pin	= 2, 
+	button_1_pin	= 3, 
+	led_red_pin		= 4, 
+	led_green_pin	= 5, 
+	buzzer_pin		= 6,
+	pressure_pin	= A0;
+
+byte button_0, button_1;
+int32_t pressure_reading_mpsi;
+bool button_0_pressed, button_1_pressed;
+uint32_t last_time, current_time, time_elapsed_ms;
+
+flood_saver::StateData globals = {};
 
 void setup() {
 	lcd.begin(16, 2);
@@ -32,44 +45,140 @@ void setup() {
 	pinMode(buzzer_pin, OUTPUT);
 	attachInterrupt(digitalPinToInterrupt(button_0_pin), button_0_interrupt, FALLING);
 	attachInterrupt(digitalPinToInterrupt(button_1_pin), button_1_interrupt, FALLING);
-	WDTCSR = (1 << WDCE);
-	WDTCSR = (1 << WDIE);
+	
+	//
+	byte watchdog_register_value = (1 << WDIE) | (1 << WDP3) | (1 << WDP0);
+	cli();
+	WDTCSR = (1 << WDCE) | (1 << WDE);
+	WDTCSR = watchdog_register_value;
+	sei();
+
+	//wdt_enable(WDTO_2S);
+
+	last_time = millis();
+	read_pressure_and_time(pressure_reading_mpsi, time_elapsed_ms);
+	
+	// Audio alarm shutoff test
+	/*
+	globals.x1 = false;
+	globals.x2 = false;
+	globals.audio_alarm_on = true;
+	globals.valve_104_open = false;
+	*/
+
+	// test1
+	globals.x1 = true;
+	// globals.water_supply_alarm_on = true;
+}
+
+struct P_t_record {
+	int32_t P_ms;
+	int32_t delta_t_ms;
+};
+
+P_t_record test1[] = {
+	{ 60000, 5001 },
+	{ 59000, 5001 },
+	{ 58000, 5001 },
+	{ 57000, 5001 },
+	{ 57000, 5001 },
+	{ 56000, 5001 },
+	{ 55000, 5001 },
+	{ 54000, 5001 },
+	{ 53000, 5001 },
+	{ 52000, 5001 },
+	{ 51000, 5001 },
+	{ 50000, 5001 },
+	{ 49000, 5001 },
+	{ 48000, 5001 },
+	{ 47000, 5001 },
+	{ 46000, 5001 },
+	{ 45000, 5001 }
+};
+
+void read_pressure_and_time(int32_t& P_mpsi, uint32_t& delta_t_ms) {
+	if (false) {
+		// This reading is not accurate right now
+		P_mpsi = 99 * static_cast<int32_t>(analogRead(pressure_pin));
+		current_time = millis();
+		delta_t_ms = current_time - last_time;
+		last_time = current_time;
+	}
+	else {
+		static uint32_t index;
+		P_t_record* test = test1;
+		index = (index + 1) % 17;
+		P_mpsi = test[index].P_ms;
+		delta_t_ms = test[index].delta_t_ms;
+	}
 }
 
 void loop() {
-	if (button_0_pressed == true) {
-		lcd.clear();
-		lcd.print("Button 0 last");
-		button_0_pressed = false;
-		lcd.setCursor(0, 1);
-		lcd.print(millis());
-	} 
-	else if (button_1_pressed == true) {
-		lcd.clear();
-		lcd.print("Button 1 last");
-		button_1_pressed = false;
-		lcd.setCursor(0, 1);
-		lcd.print(millis());
+
+	globals.x2 = button_1_pressed;
+	globals.x3 = button_0_pressed;
+	
+	read_pressure_and_time(pressure_reading_mpsi, time_elapsed_ms);
+	globals.current_pressure_mpsi = pressure_reading_mpsi;
+
+	if (globals.timer_t1_initiated) 
+		globals.timer_t1_ms += static_cast<uint32_t>(time_elapsed_ms);
+
+	if (globals.five_second_timer_t2_initiated) 
+		globals.five_second_timer_t2_ms += static_cast<uint32_t>(time_elapsed_ms);
+
+	_delay_ms(1000); //DEBUG
+	flood_saver::main_routine(globals);
+	
+	lcd.setCursor(0, 0);
+	lcd.print(time_elapsed_ms);
+	lcd.print("    ");
+	lcd.setCursor(8, 0);
+	lcd.print("S");
+	if (globals.subroutine1_called) lcd.print("1");
+	if (globals.subroutine2_called) lcd.print("2");
+	lcd.print(" ");
+	if (globals.water_supply_alarm_on) lcd.print("WS ");
+	if (globals.x4)
+		lcd.print("A");
+	else
+		lcd.print("H");
+	lcd.setCursor(0, 1);
+	lcd.print(pressure_reading_mpsi);
+	lcd.print("    ");
+	lcd.setCursor(8, 1);
+	lcd.print("T");
+	if (globals.timer_t1_initiated) lcd.print('1');
+	if (globals.five_second_timer_t2_initiated) {
+		lcd.print('2');
+		lcd.print('-');
+		lcd.print(globals.counter_ct1);
+	}
+	if (globals.leak_detected) lcd.print("!");
+	if (globals.TRACE) lcd.print('*');
+	lcd.print("    ");
+
+	if (globals.valve_104_open) {
+		digitalWrite(led_green_pin, HIGH);
 	}
 	else {
-		pressure_reading_deciP = (41 * (uint16_t)analogRead(pressure_pin) + 57) / 42;
-		char analog_reading_str[4];
-		sprintf(analog_reading_str, "%4d", pressure_reading_deciP);
-		if (analog_reading_str[2] == ' ') analog_reading_str[2] = '0';
-		lcd.setCursor(11, 1);
-		lcd.print(analog_reading_str[0]);
-		lcd.print(analog_reading_str[1]);
-		lcd.print(analog_reading_str[2]);
-		lcd.print('.');
-		lcd.print(analog_reading_str[3]);
+		digitalWrite(led_green_pin, LOW);
 	}
 
-	if (digitalRead(button_0_pin) == 0 and digitalRead(button_1_pin) == 0) {
+	if (globals.leak_alarm_on) {
+		digitalWrite(led_red_pin, HIGH);
+	}
+	else {
+		digitalWrite(led_red_pin, LOW);
+	}
+
+	if (globals.audio_alarm_on) {
 		digitalWrite(buzzer_pin, HIGH);
 	}
 	else {
 		digitalWrite(buzzer_pin, LOW);
 	}
+
 	wdt_reset();
 }
 
@@ -94,3 +203,15 @@ void button_0_interrupt() {
 void button_1_interrupt() {
 	button_1_pressed = true;
 }
+
+/*
+char analog_reading_str[4];
+sprintf(analog_reading_str, "%4d", pressure_reading_deciP);
+if (analog_reading_str[2] == ' ') analog_reading_str[2] = '0';
+lcd.setCursor(11, 1);
+lcd.print(analog_reading_str[0]);
+lcd.print(analog_reading_str[1]);
+lcd.print(analog_reading_str[2]);
+lcd.print('.');
+lcd.print(analog_reading_str[3]);
+*/
--- FloodSaver/from_plc.hpp
+++ FloodSaver/from_plc.hpp
@@ -11,7 +11,7 @@ namespace flood_saver {
 
 		uint8_t counter_ct1;
 
-		uint16_t five_second_timer_t2_ms;
+		uint32_t five_second_timer_t2_ms;
 		bool five_second_timer_t2_initiated;
 
 		bool leak_detected;
@@ -20,9 +20,15 @@ namespace flood_saver {
 		bool audio_alarm_on;
 		bool water_supply_alarm_on;
 
+		bool TRACE;
+
 		int32_t current_pressure_mpsi;
 		int32_t m1_mpsi;
 		int32_t m2_mpsi;
+
+		// Timers will reset to 0 if subroutine2 || subroutine2 !called
+		bool subroutine1_called;
+		bool subroutine2_called;
 
 		bool x1; // Start switch pressed, latch on
 		bool x2; // Stop/reset switch pressed, latch on
@@ -35,6 +41,7 @@ namespace flood_saver {
 	void main_routine(StateData& data);
 
 	void subroutine1(StateData& data) {
+		data.subroutine1_called = true;
 		if (data.x2) {
 			if (data.valve_104_open) {
 				data.valve_104_open = false;
@@ -46,9 +53,11 @@ namespace flood_saver {
 		}
 		else {
 			if (data.x4) {
+				// Away mode
 				data.t1_set_time_ms = 30000;
 			}
 			else {
+				// Home mode
 				data.t1_set_time_ms = 1200000;
 			}
 			if (data.timer_t1_initiated) {
@@ -60,16 +69,20 @@ namespace flood_saver {
 	}
 
 	void subroutine2(StateData& data) {
+		data.subroutine2_called = true;
 		if (data.x2) {
 			if (data.leak_alarm_on) {
 				data.leak_alarm_on = false;
+				data.leak_detected = false;
+				data.audio_alarm_on = false;
 			}
 			data.counter_ct1 = 0;
-			data.x1 = false;
+			data.x1 = true;
 			data.x2 = false;
 		}
 		else {
 			if (data.leak_alarm_on) {
+				data.TRACE = true;
 				return;
 			}
 			else {
@@ -77,7 +90,7 @@ namespace flood_saver {
 					if (data.five_second_timer_t2_ms > 5000) {
 						data.m2_mpsi = data.current_pressure_mpsi;
 						int32_t delta_P_mpsi = (data.m2_mpsi - data.m1_mpsi) / 5;
-						if (delta_P_mpsi > -3000 and delta_P_mpsi < -300) {
+						if (delta_P_mpsi > -3000 && delta_P_mpsi < -300) {
 							// Actual system leak detected
 							data.counter_ct1 += 1;
 						} 
@@ -88,18 +101,31 @@ namespace flood_saver {
 							}
 						}
 						if (data.counter_ct1 == 10) {
+							data.leak_detected = true;
 							data.leak_alarm_on = true;
+							data.audio_alarm_on = true;
+							data.five_second_timer_t2_initiated = false;
 						}
 						else {
 							data.five_second_timer_t2_ms = 0;
 						}
 					}
 				}
+				else {
+					data.m1_mpsi = data.current_pressure_mpsi;
+					data.five_second_timer_t2_initiated = true;
+				}
 			}
 		}
 	}
 
 	void main_routine(StateData& data) {
+		// Clear timers if subroutines were !called during last scan
+		if (! (data.subroutine1_called || data.subroutine2_called)) {
+			data.timer_t1_ms = 0;
+			data.five_second_timer_t2_ms = 0;
+			data.subroutine1_called = data.subroutine2_called = false;
+		}
 		if (data.x1) {
 			if (data.current_pressure_mpsi < 45000) {
 				if (data.current_pressure_mpsi < 35000) {
@@ -114,12 +140,14 @@ namespace flood_saver {
 				}
 				if (data.timer_t1_ms > data.t1_set_time_ms) {
 					data.leak_alarm_on = true;
+					data.audio_alarm_on = true;
 					data.x1 = false;
 					data.valve_104_open = false;
 				} 
 				else {
 					if (data.leak_detected) {
 						data.leak_alarm_on = true;
+						data.audio_alarm_on = true;
 						data.x1 = false;
 						data.valve_104_open = false;
 					}
@@ -147,20 +175,21 @@ namespace flood_saver {
 				if (data.audio_alarm_on) {
 					if (data.x3) {
 						data.audio_alarm_on = false;
+						data.x3 = false;
 					}
 				}
 				return;
 			}
 		}
 		while (data.x2) {
-			if (not (data.leak_alarm_on or data.timer_t1_alarm_on)) {
+			if (! (data.leak_alarm_on || data.timer_t1_alarm_on)) {
 				break;
 			}
 			subroutine1(data);
 		}
-		if (not (data.timer_t1_ms > data.t1_set_time_ms)) {
-			if (not data.leak_detected) {
-				if (not data.current_pressure_mpsi > 65000) {
+		if (! (data.timer_t1_ms > data.t1_set_time_ms)) {
+			if (! (data.leak_detected)) {
+				if (! (data.current_pressure_mpsi > 65000)) {
 					return;
 				}
 			}
